c49fa32c546e0705ef12a843eaa1bcd3d2968dd6
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-11-20 16:53:27 -0500

    Update est_homography.m

--- PerspectiveTransformation/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
+++ PerspectiveTransformation/RoboticsPerceptionWeek2AssignmentCode/est_homography.m
@@ -10,6 +10,19 @@ function [ H ] = est_homography(video_pts, logo_pts)
 
 % YOUR CODE HERE
 H = [];
+A = zeros(8, 9);
+for i = 1 : 4
+    x = video_pts(i, 1);
+    y = video_pts(i, 2);
+    x_prime = logo_pts(i, 1);
+    y_prime = logo_pts(i, 2);
+    A(2 * i - 1, :) = [-x, -y, -1, 0, 0, 0, x * x_prime, y * x_prime, x_prime];
+    A(2 * i, :) = [0, 0, 0, -x, -y, -1, x * y_prime, y * y_prime, y_prime];
+end
+[U, S, V] = svd(A);
+H = V(:, end);
+H = reshape(H, 3, 3);
+H = H';
 
 end
 
